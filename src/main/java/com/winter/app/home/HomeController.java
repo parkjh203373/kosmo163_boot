8913ca5f4659c4140c5298f430f8bd63ed53896e
parkjh203373 <parkjh203373@gmail.com>
2026-04-02 17:42:46 +0900

    init2

--- src/main/java/com/winter/app/home/HomeController.java
+++ src/main/java/com/winter/app/home/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String home() {		
 		
-		return "/WEB-INF/views/index.jsp";
+		return "index";
 	}
 
 }
