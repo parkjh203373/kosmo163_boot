b4a0634639db980a1b83588a62dca06f6ac1b925
parkjh203373 <parkjh203373@gmail.com>
2026-04-08 16:57:48 +0900

    init9

--- src/main/java/com/winter/app/professor/ProfessorController.java
+++ src/main/java/com/winter/app/professor/ProfessorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +15,7 @@ public class ProfessorController {
 	@Autowired
 	private ProfessorService professorService;
 	
+	@GetMapping("list")
 	public String list(Model model) throws Exception {
 		List<ProfessorDTO> ar = professorService.list();
 		
